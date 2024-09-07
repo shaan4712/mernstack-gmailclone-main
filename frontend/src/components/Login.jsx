aa2e5c53ae2f4df0c0841fa5e6331677233a3bf9
Shantanu Atmakuri <shaan2k3@gmail.com>
2024-09-07 17:14:38 +0530

    ch

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
 
     return (
         <div className='flex items-center justify-center w-screen h-screen'>
-            <form onSubmit={submitHandler} className='flex flex-col gap-3 bg-white p-4 w-[20%]'>
+            <form onSubmit={submitHandler} className='flex flex-col gap-3 bg-white p-4 w-[20%] shadow-xl'>
                 <h1 className='font-bold text-2xl uppercase my-2'>Login</h1>
                 <input onChange={changeHandler} value={input.email} name="email" type='email' placeholder='Email' className='border border-gray-400 rounded-md px-2 py-1' />
                 <input onChange={changeHandler} value={input.password} name="password" type='password' placeholder='Password' className='border border-gray-400 rounded-md px-2 py-1' />
